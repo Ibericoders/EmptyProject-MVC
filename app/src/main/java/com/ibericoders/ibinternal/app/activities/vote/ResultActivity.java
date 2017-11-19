5e9a3fcbc7bb70b549473f272c3855e81f94178d
Jorge Roldan <jorge.roldan.real@everis.com>
2017-11-19 18:17:33 +0100

    Cambios al gráfico

--- app/src/main/java/com/ibericoders/ibinternal/app/activities/vote/ResultActivity.java
+++ app/src/main/java/com/ibericoders/ibinternal/app/activities/vote/ResultActivity.java
@@ -4,48 +4,69 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 import com.ibericoders.ibinternal.R;
+import com.ibericoders.ibinternal.app.activities.generics.InflatedActivity;
 import com.ibericoders.ibinternal.app.managers.vote.MyValueFormatter;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ResultActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.OnClick;
 
+public class ResultActivity extends InflatedActivity {
+
+    /*
+     * Atributos de UI.
+     */
+    @BindView(R.id.pieChart) PieChart pieChart;
+
+    /*
+     * Atributos de negocio
+     */
     private float votesInA;
     private float votesInB;
     private float votesInC;
     private float votesInD;
     private String topic;
-    private PieChart pieChart;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
-        Button buttonShare = findViewById(R.id.buttonToShare);
-        pieChart = findViewById(R.id.pieChart);
 
-        //Llamamos a través de la clase Bundle a los datos de la votación.
-        Bundle bundle = getIntent().getExtras();
-        if (bundle != null) {
-            //int totalParticipants = bundle.getInt("totalParticipants");
-            votesInA = bundle.getInt("votesA");
-            votesInB = bundle.getInt("votesB");
-            votesInC = bundle.getInt("votesC");
-            votesInD = bundle.getInt("votesD");
-            topic = bundle.getString("topic");
+        initAttrs();
+        inflateView();
+        fillView();
+        initListeners();
+    }
+
+    @Override
+    protected void initAttrs() {
+
+        //Llamamos a través de la clase Bundle a los datos de la votación
+
+        if (this.getIntent() != null) {
+
+            votesInA = this.getIntent().getIntExtra("votesA", 0);
+            votesInB = this.getIntent().getIntExtra("votesB", 0);
+            votesInC = this.getIntent().getIntExtra("votesC", 0);
+            votesInD = this.getIntent().getIntExtra("votesD", 0);
+            topic = this.getIntent().getStringExtra("topic");
         }
 
         //creamos un array con los resultados para poder utilizarlo en la tabla.
@@ -55,42 +76,66 @@ public class ResultActivity extends AppCompatActivity {
         resultVotes[2] = votesInC;
         resultVotes[3] = votesInD;
 
-        List<PieEntry> entries = new ArrayList<>();
+    }
 
+    @Override
+    protected void fillView() {
+
+    }
+
+    @Override
+    protected void initListeners() {}
+
+
+    private void formatData(){
+        List<PieEntry> entries = new ArrayList<>();
         // transformamos nuestros datos en PieEntry.
         entries.add(new PieEntry(votesInA, "Opción A / SÍ"));
         entries.add(new PieEntry(votesInB, "Opción B/ NO"));
         entries.add(new PieEntry(votesInC, "Opción C"));
         entries.add(new PieEntry(votesInD, "Opción D"));
 
-        //seteamos los datos en la tabla y sus características.
         PieDataSet set = new PieDataSet(entries, "SetPieChart");
-        PieData data = new PieData(set);
-        pieChart.setData(data);
-        pieChart.setCenterText(topic);
-        set.setSliceSpace(7f);
+        set.setSliceSpace(2f);
         set.setSelectionShift(7f);
-        pieChart.invalidate(); // refresh
-
-
         // seteamos los colores del grafico y el color del texto, entre otros parámetros.
         set.setColors(new int[]{R.color.colorPrimaryLight, R.color.colorButtonNo, R.color.colorButtonC, R.color.colorButtonD}, this);
+
+        PieData data = new PieData(set);
         data.setValueFormatter(new MyValueFormatter());
         data.setValueTextSize(15f);
         data.setValueTextColor(Color.BLACK);
-        pieChart.setEntryLabelColor(Color.BLACK);
-        buttonShare.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                pieChart.saveToGallery(topic, 100);
-                sendEmail();
-            }
-        });
 
+        pieChart.setData(data);
+        configChart();
+    }
+
+    private void configChart(){
+
+        pieChart.setBackgroundColor(Color.TRANSPARENT);
+        pieChart.setDrawEntryLabels(true);
+        pieChart.setUsePercentValues(false);
+        pieChart.getLegend().setEnabled(false);
+        pieChart.setHighlightPerTapEnabled(false);
+        pieChart.setDescription(null);
+        pieChart.setCenterText(topic);
+        pieChart.setEntryLabelColor(Color.BLACK);
+        pieChart.setDrawHoleEnabled(false);
+        pieChart.setRotationEnabled(false);
+        pieChart.setMaxAngle(180f);
+        pieChart.animateY(1400, Easing.EasingOption.EaseInOutQuad);
+        pieChart.setEntryLabelTextSize(18);
+        pieChart.invalidate();
+    }
+
+    @OnClick(R.id.buttonToShare)
+    public void share(){
+        pieChart.saveToGallery(topic, 100);
+        sendEmail();
 
     }
 
-   /* @Override
+    /* @Override
     public void onRequestPermissionsResult(int requestCode,
                                            String permissions[], int[] grantResults) {
         switch (requestCode) {
